4bd19c5430e2462de3ca59659e0791434a6c2bce
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-04-30 11:17:54 +0530

    removed the dist in the vite Config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Explicitly define output directory (Vercel should be configured to use this)
-  },
+  // Leave 'build.outDir' as default (which is 'dist')
 })
