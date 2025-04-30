27411717781a461b79c8c882d5642113659d265d
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-04-30 13:02:50 +0530

    the dist is addes and vite is modified

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Leave 'build.outDir' as default (which is 'dist')
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
