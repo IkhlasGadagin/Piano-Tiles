7e7025fbeac5d7a42c33724560021038a4b6a6cf
IkhlasGadagin <ikhlashgadagin@gmail.com>
2025-04-30 10:56:41 +0530

    the vite config is modified for the build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Explicitly define output directory (Vercel should be configured to use this)
+  },
 })
